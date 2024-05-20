6086704266b876f213583765554e67d3d328ec98
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2024-05-19 21:57:23 -0300

    Api

--- API/src/main/java/itiroBeto/com/github/APIHospital/enums/ConsultationPatientStatusEnum.java
+++
@@ -1,8 +0,0 @@
-package itiroBeto.com.github.APIHospital.enums;
-
-public enum ConsultationPatientStatusEnum {
-
-    CONFIRMADA,
-    REALIZADA,
-    CANCELADA
-}
--- API/src/main/java/itiroBeto/com/github/APIHospital/enums/ExamsPatientStatusEnum.java
+++
@@ -1,8 +0,0 @@
-package itiroBeto.com.github.APIHospital.enums;
-
-public enum ExamsPatientStatusEnum {
-
-    PENDENTE,
-    REALIZADO,
-    CANCELADO
-}
